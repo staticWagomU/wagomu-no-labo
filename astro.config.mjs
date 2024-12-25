6ae1d7a90c7ddf05cb12b6c033feb249d5214c80
staticWagomU <to6wa17@gmail.com>
2024-12-25 22:12:45 +0900

    chore: add react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://labo.wagomu.me',
   adapter: cloudflare(),
+  integrations: [react()],
 });
