1af86803ea430b331b78b6c5f4057bc88befcaa5
staticWagomU <to6wa17@gmail.com>
2024-12-26 09:07:12 +0900

    feat!: migrate to deno

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true
-    }
-  }),
-
   integrations: [react()]
 });
